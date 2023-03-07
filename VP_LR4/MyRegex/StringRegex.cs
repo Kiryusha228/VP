3131718afc98daf76c25ec67455892fc7ca716ac
Kiryusha228 <kirya.abbasov.02@mail.ru>
2023-03-07 15:52:19 +0300

    lLR4

---
+++ VP_LR4/MyRegex/StringRegex.cs
@@ -0,0 +1,75 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace MyRegex
+{
+    public static class StringRegex
+    {
+        public static int GetPostalIndex(string str)
+        {
+            Regex rg = new Regex(@"\b\d{6}\b");
+            MatchCollection matchedGroups = rg.Matches(str);
+            return matchedGroups.Count;
+        }
+
+        public static string SwapLetters(string str)
+        {
+            return Regex.Replace(str, @"\b([а-я])([а-я]+)([а-я])\b", "$3$2$1", RegexOptions.IgnoreCase);
+        }
+
+        public static string HidePhoneNumbers(string str)
+        {
+            //Regex rg = new Regex(@"[+][7][(]\d{3,5}[)]((\d{3}[-]\d{2}[-]\d{2})|(\d{3}[-]\d{3})|(\d{3}[-]\d{2}))");
+            //string pattern = @"(?<tag1>[+][7][(]\d{3,5}[)]\d)\d{2}[-](((?<tag2>\d{2}[-]\d{2}))|((?<tag3>\d{3}))|((?<tag4>\d{2})))";
+            //var reg = new Regex(pattern);
+            //return Regex.Replace(str, pattern, "${tag1}xx-${tag2}${tag3}${tag4}");
+            string pattern1 = @"([+][7][(]\d{3,5}[)]\d)\d{2}[-]\d{2}[-]\d{2}";
+            string pattern2 = @"([+][7][(]\d{3,5}[)]\d)\d{2}[-]\d{3}";
+            string pattern3 = @"([+][7][(]\d{3,5}[)]\d)\d{2}[-]\d{2}";
+            string replacement1 = "$1xx-xx-xx";
+            string replacement2 = "$1xx-xxx";
+            string replacement3 = "$1xx-xx";
+            str = Regex.Replace(str, pattern1, replacement1);
+            str = Regex.Replace(str, pattern2, replacement2);
+            return Regex.Replace(str, pattern3, replacement3);
+        }
+
+        public static string GetCarNumbers(string str)
+        {
+            Regex rg = new Regex(@"\b[АВЕКМНОРСТУХ]{1}\d{3}[АВЕКМНОРСТУХ]{2}((\d{3})|(\d{2}))\b");
+            MatchCollection matchedGroups = rg.Matches(str);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < matchedGroups.Count; i++)
+            {
+                sb.Append($"{matchedGroups[i].Value}, ");
+            }
+            return sb.ToString();
+        }
+        //sb
+        //snils
+
+        public static string GetIPv4(string str)
+        {
+            Regex rg = new Regex(@"\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b");
+            MatchCollection matchedGroups = rg.Matches(str);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < matchedGroups.Count; i++)
+            {
+                sb.Append($"{matchedGroups[i].Value}, ");
+            }
+            return sb.ToString();
+        }
+
+        public static string GetSnils(string str)
+        {
+            Regex rg = new Regex(@"\b(\d{3}-){2}\d{3} \d{2}\b");
+            MatchCollection matchedGroups = rg.Matches(str);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < matchedGroups.Count; i++)
+            {
+                sb.Append($"{matchedGroups[i].Value}, ");
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ VP_LR4/StringRegexUI/Program.cs
@@ -0,0 +1,47 @@
+﻿using MyRegex;
+
+namespace StringRegexUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Почтовые индексы: 228322, 444333, 312111, 444433, Не почтовые индексы:4, 555, 4334, 22, 33377777";
+            string str1 = "Эти слова станут странными";
+            string str2 = "Номера телефона: +7(916)348-32-23, +7(78699)853-111, +7(7167)838-32";
+            string str3 = "Мой отец катается на автобусе с номером М777СХ62, а у Димы О434ТК62; Номеров ПК332Й43 и КУ444У1024 Не существует";
+            string str4 = "IPv4: 222.3.0.1, 255.255.255.0; Не IPv4:  999.222.333.111";
+            string str5 = "Снилсы: 444-444-223 32, 333-232-112 32, 423-234-222 23, не снилсы: 233-2332-23-23 32, 32-33-434-3 23";
+
+            Console.WriteLine(str);
+            Console.Write("Количество почтовых индексов: ");
+            Console.WriteLine(StringRegex.GetPostalIndex(str));
+            Console.WriteLine();
+
+            Console.WriteLine(str1);
+            Console.Write("Новая строка:\n");
+            Console.WriteLine(StringRegex.SwapLetters(str1));
+            Console.WriteLine();
+
+            Console.WriteLine(str2);
+            Console.Write("Новая строка:\n");
+            Console.WriteLine(StringRegex.HidePhoneNumbers(str2));
+            Console.WriteLine();
+
+            Console.WriteLine(str3);
+            Console.Write("Номера машин: ");
+            Console.WriteLine(StringRegex.GetCarNumbers(str3));
+            Console.WriteLine();
+
+            Console.WriteLine(str4);
+            Console.Write("IPv4 адреса: ");
+            Console.WriteLine(StringRegex.GetIPv4(str4));
+            Console.WriteLine();
+
+            Console.WriteLine(str5);
+            Console.Write("Снилсы: ");
+            Console.WriteLine(StringRegex.GetSnils(str5));
+            Console.WriteLine();
+        }
+    }
+}
